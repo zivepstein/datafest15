a89fc93caf910b7857980b8aa0d3b1716545e6e7
Bill DeRose <bill.derose11@gmail.com>
2015-04-12 20:36:36 -0700

    Update .gitignore and add protocol for pushing/pulling branches.

--- gitTest.R
+++ gitTest.R
@@ -1,7 +1,13 @@
-library(dplyr)
-library(ggplot2)
+# When making changes, navigate to github.com/zivepstein/datafest15
+# and create a new branch online. For instance, the branch this is being created
+# on it testBranch. You should be more creative in your naming so we have
+# an idea about what is what. Then, use the RStudio git tool
+# to "Pull Branches". You can now continue with the changes you would
+# like to make. When done, select the edited files and "Commit" them. You can 
+# again do this using the git tool above. Near the upper left of the commit 
+# panel, make sure you have selected the correct branch and are NOT commiting 
+# directly to master. Once you have added your changes, use "Push Branch" to
+# push the remote repositoryand submit a pull request to be included in master.
+# Try following this workflow to add your $name below.
 
 cat("Bill")
-cat("Jacob")
-cat("Metz")
-cat("Ziv")
