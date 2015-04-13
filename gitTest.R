831f1ba58e16944d4cd84909925c5dad2b0ff0b0
Bill DeRose <bill.derose11@gmail.com>
2015-04-12 20:49:11 -0700

    Hotfix: more typos

--- gitTest.R
+++ gitTest.R
@@ -1,13 +1,13 @@
 # When making changes, navigate to github.com/zivepstein/datafest15
 # and create a new branch online. For instance, the branch this is being created
-# on it testBranch. You should be more creative in your naming so we have
+# on is testBranch. You should be more creative in your naming so we have
 # an idea about what is what. Then, use the RStudio git tool
 # to "Pull Branches". You can now continue with the changes you would
 # like to make. When done, select the edited files and "Commit" them. You can 
 # again do this using the git tool above. Near the upper left of the commit 
 # panel, make sure you have selected the correct branch and are NOT commiting 
 # directly to master. After committing all of your changes but before
-# pushing them to the remote repository, you should always "Pull Brnaches"
+# pushing them to the remote repository, you should always "Pull Branches"
 # one more time so that you get the most up-to-date master. Many a
 # merge conflict are avoided this way. Once you're all set, use "Push Branch" to
 # push to the remote repo and submit a pull request to be included in master.
