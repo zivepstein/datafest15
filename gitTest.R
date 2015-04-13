6a62ef221cece18fd0f4310379e4bfec554ff516
Bill DeRose <bill.derose11@gmail.com>
2015-04-12 20:42:40 -0700

    more git updates.

--- gitTest.R
+++ gitTest.R
@@ -6,8 +6,11 @@
 # like to make. When done, select the edited files and "Commit" them. You can 
 # again do this using the git tool above. Near the upper left of the commit 
 # panel, make sure you have selected the correct branch and are NOT commiting 
-# directly to master. Once you have added your changes, use "Push Branch" to
-# push the remote repositoryand submit a pull request to be included in master.
+# directly to master. After committing all of your changes but before
+# pushing them to the remote repository, you should always "Pull Brnaches"
+# one more time so that you get the most up-to-date master. Many a
+# merge conflict are avoided this way. Once you're all set, use "Push Branch" to
+# push to the remote repo and submit a pull request to be included in master.
 # Try following this workflow to add your $name below.
 
 cat("Bill")
