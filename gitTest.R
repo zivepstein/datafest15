4146b786ebee49c4f11121d8ea15f6bb95b64845
Bill DeRose <bill.derose11@gmail.com>
2015-04-12 20:39:10 -0700

    Fix description of git protocol

--- gitTest.R
+++ gitTest.R
@@ -1,13 +1,14 @@
 # When making changes, navigate to github.com/zivepstein/datafest15
-# and create a new branch online. For instance, the branch this is being created
-# on it testBranch. You should be more creative in your naming so we have
-# an idea about what is what. Then, use the RStudio git tool
-# to "Pull Branches". You can now continue with the changes you would
+# and create a new branch online. For instance, this was created
+# on a branch called testBranch. You should be more creative in your naming 
+# so we have an idea about what it is. Then, use the RStudio git tool
+# to "Pull Branches"; this pulls the remote branch to your local machine 
+# where you can make changes to it. Continue with the changes you would
 # like to make. When done, select the edited files and "Commit" them. You can 
 # again do this using the git tool above. Near the upper left of the commit 
 # panel, make sure you have selected the correct branch and are NOT commiting 
 # directly to master. Once you have added your changes, use "Push Branch" to
-# push the remote repositoryand submit a pull request to be included in master.
+# push the remote repository and submit a pull request to be included in master.
 # Try following this workflow to add your $name below.
 
 cat("Bill")
